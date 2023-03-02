d4a298fb13bf88110ddd2686cb2eb4cc63d71906
Kovács Dorina <kovacsdorina2@gmail.com>
2023-03-02 15:52:16 +0100

    api elérése sikeres | service megírása kész | students.ts megírása kész | megjelenites mukodik

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { StudentsComponent } from './students/students.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,34 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
+  host ="http://localhost:3000/";
+
+  constructor(private http: HttpClient) { }
+
+  getStudents(){
+
+    let headers = new HttpHeaders({
+      'Content-Type' : 'application/json'
+    })
+
+    let httpOption = {
+      headers: headers
+    }
+
+    let endpoint = "students";
+    let url = this.host + endpoint;
+    return this.http.get<any>(url, httpOption);
+  }
+
+  getScores(){
+     let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+     })
+     let httpOption = {
+      headers: headers
+     }
+     let endpoint = "scores";
+     let url = this.host + endpoint;
+     return this.http.get<any>(url, httpOption);
+  }
 }
