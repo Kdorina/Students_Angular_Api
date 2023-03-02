02f572ec68146fcf0caa01f2805acc94b725f3c7
Kovács Dorina <kovacsdorina2@gmail.com>
2023-03-02 17:14:52 +0100

    formázás

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 
 const routes: Routes = [
-  {path:'students', component: StudentsComponent}
+  {path:'students', component: StudentsComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'}
 ];
 
 @NgModule({
